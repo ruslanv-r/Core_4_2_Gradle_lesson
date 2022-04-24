15a3493f0282299cc2e3ea8e82e68c39bef35423
Romanyuk Ruslan <rvregi@gmail.com>
2022-04-24 14:22:20 +0900

    Initial commit

---
+++ service/src/main/java/MyService.java
@@ -0,0 +1,26 @@
+import java.util.UUID;
+
+public class MyService {
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db;
+
+    public MyService() {
+        db = new Db(dbSetting);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
